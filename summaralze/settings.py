c0a1f7c4839f1e1fc2d56b6c38d716ebaf9ebc58
Alperen Çoban <alperencoban474@gmail.com>
2025-04-03 19:59:43 +0300

    Update database settings for better environment handling

--- summaralze/settings.py
+++ summaralze/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'summaralze.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Render PostgreSQL Database
